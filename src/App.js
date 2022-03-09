dff877ba1cedc110fc9255039d623d1446ec44b6
ratnabh-classic <ratnabh.rai@classicinformatics.com>
2022-03-09 12:44:02 +0530

    added Router for BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import "./App.css";
 import {
-  BrowserRouter as Router,
+  Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
 import React, { Suspense } from "react";
 
-import Login from "./containers/login";
+import Login from "./containers/loginPage";
+import Signup from "./containers/SignupPage";
 import AuthContextProvider from "./contexts/AuthContext";
 import routes from "./routes";
 import AuthRoute from "./components/auth-route";
-
+import { history } from "./index";
 import Header from "./components/header/index";
 function App() {
   return (
     <AuthContextProvider>
       <div className="App">
-        <Header />
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <div>
+              <Header />
+              <Suspense fallback={<React.Fragment />}>
+                {routes.map((route: Object, index: number) => {
+                  return route.authRequired ? (
+                    <AuthRoute
+                      component={route.component}
+                      exact={true}
+                      key={index}
+                      name={route.name}
+                      path={route.path}
+                      title={route.title}
+                    />
+                  ) : (
+                    <Route
+                      component={route.component}
+                      exact
+                      key={index}
+                      name={route.name}
+                      path={route.path}
+                      title={route.title}
+                    />
+                  );
+                })}
+                <Redirect to="/not-found" />
+              </Suspense>
+            </div>
+          </Switch>
+        </Router>
       </div>
     </AuthContextProvider>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
 import App from "./App";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { store, persistor } from "./redux/store";
